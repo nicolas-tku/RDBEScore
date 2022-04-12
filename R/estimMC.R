2710bea6ab7090a2b7974a25db7f1f45bacb1a0e
Richard Meitern <richard.meitern@ut.ee>
2022-04-12 15:57:26 +0300

    added tests

--- R/estimMC.R
+++ R/estimMC.R
@@ -5,36 +5,69 @@
 #' @param total total number of units int the population
 #' @param method selection method code e.g SRSWOR
 #'
-#' @return list of 2 the population total and its variance
+#' @return list of 5 elements including the population mean, total
+#' (and their variance) and the I order inclusion probabilities
 #' @export
 #'
 #' @examples
-#' estimMC(c(3,4,4,5),c(4,4,4,4),c(8,8,8,8))
-estimMC<-function(y, sampled, total, method="SRSWOR"){
+#' estimMC(c(3, 4, 4, 5), c(4, 4, 4, 4), c(8, 8, 8, 8))
+estimMC <- function(y, sampled, total, method = "SRSWOR") {
+  implementedMethods <- c(
+    "^SRSWR$",
+    "^SRSWOR$",
+    "^CENSUS$"
+  )
+  if (!any(sapply(implementedMethods, grepl, method))) {
+    stop(paste("Sampling method ", method, "not implemented!"))
+  }
 
   n <- length(y)
 
-  if(any(length(sampled) != n, length(total) != n)){
+  if (any(length(sampled) != n, length(total) != n)) {
     stop("y, sampled and total must, be vectors of same length!")
   }
-  if(method == "SRSWOR"){
-    pk <- sampled/total
-    PI<-matrix(sampled/total*(sampled-1)/(total-1),
-               nrow = length(pk),
-               ncol = length(pk))
-    diag(PI)<-pk
-    A <- ( PI -  ( pk %*% t(pk)))/( PI * ( pk %*% t(pk))) * ( y %*% t(y))
-    var <- sum(A)
-    return(list(total=sum(y/pk),var=var))
+  if (any(grepl(method, implementedMethods))) {
+    pk <- sampled / total
+    PI <- matrix(sampled / total * (sampled - 1) / (total - 1),
+      nrow = length(pk),
+      ncol = length(pk)
+    )
+    diag(PI) <- pk
+    pl <- t(pk)
+    A <- (PI - (pk %*% pl)) / (PI * (pk %*% pl)) * (y %*% t(y))
 
+    total.est <- sum(y / pk)
+    var.total <- sum(A)
+
+    mean.est <- sum(y / pk / total)
+    var.mean <- sum(A / total)
   }
-  if(method == "SRSWR"){
+  # #TODO Is this SRSWR necessary?  This is just a simplification of SRSWOR
+  # if(method == "SRSWR") {
+  #   pk <- 1/total
+  #   mean.est <-  (1/n) * sum(y/pk/total)
+  #   total.est <- (1/n) * sum(y/pk)
+  #   A <- (y/pk - total.est)^2
+  #
+  #   var.total <- (1/(n-1))*sum(A/total)
+  #   var.mean <- (1/(n-1))*sum(A/(total^2))
+  #   pk<-pk*n
+  # }
+  # #TODO Is CENSUS necessary?  This is just a simplification of SRSWOR at n=N
+  # if(method == "CENSUS") {
+  #   total.est <- sum(y)
+  #   var.total <- 0
+  #   mean.est <- sum(y)/n
+  #   var.mean <- 0
+  #   pk <- rep(1,n)
+  #
+  # }
 
-  }
-
-  stop(paste("The implemented methods do not include", method))
-
-
-
+  return(list(
+    est.total = total.est,
+    est.mean = mean.est,
+    var.total = var.total,
+    var.mean = var.mean,
+    PI = PI
+  ))
 }
-
---
+++ tests/testthat/test-estimMC.R
@@ -0,0 +1,104 @@
+
+### --------- all elements in sample-------------
+testMain <- "all elements in sample"
+test_that(paste0("total variance is 0 if", testMain), {
+  tot <- 4
+  items <- c(3, 4, 4, 5)
+  elems <- length(items)
+  x <- estimMC(items, rep(elems, elems), rep(tot, elems))
+  expect_equal(x$var.total, 0)
+})
+
+test_that(paste("mean variance is 0 if", testMain), {
+  tot <- 4
+  items <- c(3, 4, 4, 5)
+  elems <- length(items)
+  x <- estimMC(items, rep(elems, elems), rep(tot, elems))
+  expect_equal(x$var.mean, 0)
+})
+
+test_that(paste("correct total if", testMain), {
+  tot <- 4
+  items <- c(3, 4, 4, 5)
+  elems <- length(items)
+  x <- estimMC(items, rep(elems, elems), rep(tot, elems))
+  expect_equal(x$est.total, sum(items))
+
+  items <- c(3, 1, 4, 2)
+  x <- estimMC(items, rep(elems, elems), rep(tot, elems))
+  expect_equal(x$est.total, sum(items))
+})
+
+test_that(paste("correct mean if", testMain), {
+  tot <- 4
+  items <- c(3, 4, 4, 5)
+  elems <- length(items)
+  x <- estimMC(items, rep(elems, elems), rep(tot, elems))
+  expect_equal(x$est.mean, mean(items))
+
+  items <- c(3, 1, 4, 2)
+  x <- estimMC(items, rep(elems, elems), rep(tot, elems))
+  expect_equal(x$est.mean, sum(items) / elems)
+})
+
+### --------- half of elements in sample-------------
+testMain <- "half of elements in sample"
+test_that(paste("total variance is correct if", testMain), {
+  tot <- 8
+  items <- c(3, 4, 4, 5)
+  elems <- length(items)
+  x <- estimMC(items, rep(elems, elems), rep(tot, elems))
+  expect_equal(x$var.total, 5 + 1 / 3)
+})
+
+test_that(paste("mean variance is correct if", testMain), {
+  tot <- 8
+  items <- c(3, 4, 4, 5)
+  elems <- length(items)
+  x <- estimMC(items, rep(elems, elems), rep(tot, elems))
+  expect_equal(x$var.mean, 2 / 3)
+})
+
+test_that(paste("correct total if", testMain), {
+  tot <- 8
+  items <- c(3, 4, 4, 5)
+  elems <- length(items)
+  x <- estimMC(items, rep(elems, elems), rep(tot, elems))
+  expect_equal(x$est.total, sum(items) * 2)
+
+  items <- c(3, 1, 4, 2)
+  x <- estimMC(items, rep(elems, elems), rep(tot, elems))
+  expect_equal(x$est.total, sum(items) * 2)
+})
+
+test_that(paste("correct mean if", testMain), {
+  tot <- 8
+  items <- c(3, 4, 4, 5)
+  elems <- length(items)
+  x <- estimMC(items, rep(elems, elems), rep(tot, elems))
+  expect_equal(x$est.mean, mean(items))
+
+  items <- c(3, 1, 4, 2)
+  x <- estimMC(items, rep(elems, elems), rep(tot, elems))
+  expect_equal(x$est.mean, sum(items) / elems)
+})
+
+### --------- errors-------------
+testMain <- "error"
+test_that(paste("unequal probality sampling is not implemented"), {
+  tot <- 4
+  items <- c(3, 4, 4, 5)
+  elems <- length(items)
+  expect_error(estimMC(items, rep(elems, elems), rep(tot, elems),
+                       method = "UPSWOR"))
+  expect_error(estimMC(items, rep(elems, elems), rep(tot, elems),
+                       method = "UPSWR"))
+})
+
+test_that(paste("all imputs must have same length"), {
+  tot <- 4
+  items <- c(3, 4, 4, 5)
+  elems <- length(items)
+  expect_error(estimMC(items, elems, rep(tot, elems)))
+  expect_error(estimMC(items, rep(elems, elems), tot))
+})
