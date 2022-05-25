2ea2791de89232e59e1d329109318520aa808e73
David Currie <davidcurrie2001@yahoo.co.uk>
2022-05-25 12:32:38 +0100

    Added doEstimationForAllStrata function and test

---
+++ tests/testthat/test-doEstimationForAllStrata.R
@@ -0,0 +1,25 @@
+test_that("doEstimationForAllStrata can run on an object from an H1 data extract
+          without warnings or errors",  {
+
+  myPath <- ".\\h1_v_1_19"
+  myH1RawObject <-
+     createRDBESRawObject(rdbesExtractPath = myPath)
+
+  # Update our test data with some random sample measurements
+  myH1RawObject[["SA"]]$SAsampWtLive <-
+    round(runif(n = nrow(myH1RawObject[["SA"]]), min = 1, max = 100))
+
+  expect_warning(doEstimationForAllStrata(
+                                        rdbesRawObjectForEstim = myH1RawObject,
+                                        hierarchyToUse = 1,
+                                        verbose = FALSE),
+                                      NA)
+
+  expect_error(doEstimationForAllStrata(
+                                        rdbesRawObjectForEstim = myH1RawObject,
+                                        hierarchyToUse = 1,
+                                        verbose = FALSE),
+                                    NA)
+
+
+})
