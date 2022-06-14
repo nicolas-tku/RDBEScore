ca46109f4be0b54eb0f6053c680a004d1ed849d0
David Currie <davidcurrie2001@yahoo.co.uk>
2022-06-14 16:19:25 +0100

    Changing to use su style colun names

--- R/createRDBESEstObject.R
+++ R/createRDBESEstObject.R
@@ -17,6 +17,15 @@
 createRDBESEstObject <- function(rdbesPrepObject, hierarchyToUse) {
   warning("Function is a work in progress and not fully tested yet")
 
+
+  # For testing
+  #myH1RawObject <-
+  #  createRDBESRawObject(rdbesExtractPath = "tests/testthat/h1_v_1_19")
+  #rdbesPrepObject <- myH1RawObject
+  #hierarchyToUse <- 1
+
+
+
   if (!validateRDBESRawObject(rdbesPrepObject, verbose = FALSE)) {
     stop(paste0(
       "rdbesPrepObject is not valid ",
@@ -35,152 +44,19 @@ createRDBESEstObject <- function(rdbesPrepObject, hierarchyToUse) {
     stop("Sub-sampling present in SA: not yet developed")
   }
 
-  # Copy the inpput data table so we don't accidentally change the orignal data
+  # Copy the input data table so we don't accidentally change the orignal data
   rdbesPrepObjectCopy <- data.table::copy(rdbesPrepObject)
 
-  # Which tables do we expect to find in the data?
-  targetTables <- icesRDBES::tablesInRDBESHierarchies[[hierarchyToUse]]
+  # Combine the lower hierachy tables (FM,BV)
+  allLower <- processRDBESEstObjLowerHierarchies(rdbesPrepObjectCopy)
 
-  # Break out the lower hierachies - they need to join to different tables
-  lowerA <- rdbesPrepObjectCopy[["SA"]][
-    (rdbesPrepObjectCopy[["SA"]][, "SAlowHierarchy"] == "A")[, 1],
-  ]
-  lowerB <- rdbesPrepObjectCopy[["SA"]][
-    (rdbesPrepObjectCopy[["SA"]][, "SAlowHierarchy"] == "B")[, 1],
-  ]
-  lowerC <- rdbesPrepObjectCopy[["SA"]][
-    (rdbesPrepObjectCopy[["SA"]][, "SAlowHierarchy"] == "C")[, 1],
-  ]
-  lowerD <- rdbesPrepObjectCopy[["SA"]][
-    (rdbesPrepObjectCopy[["SA"]][, "SAlowHierarchy"] == "D")[, 1],
-  ]
+  # Combine the upper hierachy tables
+  upperHierarchy <- processRDBESEstObjUpperHierarchies(
+                                        rdbesPrepObject = rdbesPrepObjectCopy,
+                                        hierarchyToUse = hierarchyToUse)
 
-  # Join FM and BV - we do it in both ways left join on FM and right join on
-  # BV.  this is because not all of the lower hierachies have FM data
-
-  fMBV <-
-    dplyr::left_join(rdbesPrepObjectCopy[["FM"]],
-      rdbesPrepObjectCopy[["BV"]],
-      by = "FMid"
-    )
-  # sort out the wrong SAid column name after the join
-  names(fMBV)[names(fMBV) == "SAid.x"] <- "SAid"
-  fMBV[, "SAid.y" := NULL]
-
-  # Now get rid of any unneccessary id fields to avoid trouble with field naming
-  allowedIds <- c("SAid", "FMid", "BVid")
-  unallowedIDs <-
-    paste0(names(rdbesPrepObjectCopy), "id")[
-      !(paste0(names(rdbesPrepObjectCopy), "id") %in% allowedIds)
-    ]
-  unallowedIDs <-
-    names(fMBV)[names(fMBV) %in% unallowedIDs]
-  if (length(unallowedIDs) > 0) fMBV[, (unallowedIDs) := NULL]
-
-
-  bVFM <- dplyr::right_join(rdbesPrepObjectCopy[["FM"]],
-    rdbesPrepObjectCopy[["BV"]],
-    by = "FMid"
-  )
-  # sort out the wrong SAid column name after the join
-  names(bVFM)[names(bVFM) == "SAid.y"] <- "SAid"
-  bVFM[, "SAid.x" := NULL]
-
-  # Now get rid of any unneccessary id fields to avoid trouble with field naming
-  allowedIds <- c("SAid", "FMid", "BVid")
-  unallowedIDs <-
-    paste0(names(rdbesPrepObjectCopy), "id")[
-      !(paste0(names(rdbesPrepObjectCopy), "id") %in% allowedIds)
-    ]
-  unallowedIDs <-
-    names(bVFM)[names(bVFM) %in% unallowedIDs]
-  if (length(unallowedIDs) > 0) bVFM[, (unallowedIDs) := NULL]
-
-  lowerA <- dplyr::left_join(lowerA, fMBV, by = "SAid")
-  lowerB <- dplyr::left_join(lowerB, fMBV, by = "SAid")
-  # Note the difference in lowerC
-  lowerC <- dplyr::left_join(lowerC, bVFM, by = "SAid")
-  lowerD <- dplyr::left_join(lowerD, fMBV, by = "SAid")
-
-  # Ok, this shoudl be all the SA, FM, and BV data for all lower hierarchies
-  allLower <- rbind(lowerA, lowerB, lowerC, lowerD)
-
-  myRDBESEstObj <- NA
-
-  for (i in 1:(length(targetTables) - 1)) {
-    if (i > 1) {
-      joinField <- paste0(targetTables[i - 1], "id")
-    } else {
-      joinField <- NA
-    }
-    thisTable <- targetTables[i]
-
-    breakLoop <- FALSE
-
-    if (thisTable == "SA") {
-      myRDBESEstObj <-
-        dplyr::left_join(myRDBESEstObj, allLower, by = joinField)
-      breakLoop <- TRUE
-      # break
-    } else if (i == 1) {
-      myRDBESEstObj <- rdbesPrepObjectCopy[[thisTable]]
-    } else if (i >= 2) {
-
-      # First get rid of any unneccessary id fields to avoid trouble
-      # with field naming
-      allowedIds <- c(paste0(thisTable, "id"), joinField, "VDid", "SLid")
-      unallowedIDs <-
-        paste0(names(rdbesPrepObjectCopy), "id")[
-          !(paste0(names(rdbesPrepObjectCopy), "id") %in% allowedIds)
-        ]
-      unallowedIDs <-
-        names(rdbesPrepObjectCopy[[thisTable]])[
-          names(rdbesPrepObjectCopy[[thisTable]]) %in% unallowedIDs
-        ]
-      if (length(unallowedIDs) > 0) {
-        rdbesPrepObjectCopy[[thisTable]][, (unallowedIDs) := NULL]
-      }
-
-      myRDBESEstObj <-
-        dplyr::left_join(myRDBESEstObj,
-          rdbesPrepObjectCopy[[thisTable]],
-          by = joinField
-        )
-    }
-
-    # Need to do this other we get a warning message from set()
-    data.table::setDT(myRDBESEstObj)
-
-    # Add a column with the previous table ID so that we can find it easily
-    if (i == 1) {
-      data.table::set(myRDBESEstObj,
-        j = (paste0(thisTable, "prevTableID")), value = NA
-      )
-    } else {
-      data.table::set(myRDBESEstObj,
-        j = (paste0(thisTable, "prevTableID")),
-        value = myRDBESEstObj[, joinField, with = FALSE]
-      )
-    }
-
-    if (thisTable != "SD") {
-      # Add a column with the stratum name concatenated with the parent ID -
-      # strata need to be considered in respect of their parent record
-      data.table::set(myRDBESEstObj,
-        j = (paste0(thisTable, "stratumNameFull")),
-        value = paste0(
-          myRDBESEstObj[, get(paste0(thisTable, "prevTableID"))],
-          ":",
-          myRDBESEstObj[, get(paste0(thisTable, "stratumName"))]
-        )
-      )
-    }
-
-    if (breakLoop) break
-  }
-
-
-  names(myRDBESEstObj)
+  # Join the upper and lower hierarchy tables together
+  myRDBESEstObj <- dplyr::left_join(upperHierarchy,allLower,by="SAid")
 
   # Choose which VDid field to keep
   vdIDFields <- names(myRDBESEstObj)[grepl("^VDid", names(myRDBESEstObj))]
@@ -194,13 +70,195 @@ createRDBESEstObject <- function(rdbesPrepObject, hierarchyToUse) {
         break
       }
     }
+    # Rename the VDid field we want to keep
     if (!is.na(vdIDFieldToKeep)) {
       names(myRDBESEstObj)[names(myRDBESEstObj) == vdIDFieldToKeep] <- "VDid"
+      vdIDFieldsToRemove <- vdIDFields[!(vdIDFields == vdIDFieldToKeep)]
+    } else {
+      vdIDFieldsToRemove <- vdIDFields
     }
+    # Get rid of the other VDid fields
+    myRDBESEstObj[, (vdIDFieldsToRemove) := NULL]
   }
 
   # Set the class of the object
   class(myRDBESEstObj) <- c("RDBESEstObject", class(myRDBESEstObj))
 
+  names(myRDBESEstObj)
   myRDBESEstObj
+}
+
+#' Private function to process the lower hierarchies when creating
+#' the RDBESEstObject
+#'
+#' @param rdbesPrepObject
+#'
+#' @return allLower - the FM and BV tables combined
+#'
+#' @examples
+processRDBESEstObjLowerHierarchies <- function(rdbesPrepObject){
+
+  # Break out the lower hierachies - they need to join to different tables
+  lowerA <- rdbesPrepObject[["SA"]][
+    (rdbesPrepObject[["SA"]][, "SAlowHierarchy"] == "A")[, 1],"SAid"
+  ]
+  lowerB <- rdbesPrepObject[["SA"]][
+    (rdbesPrepObject[["SA"]][, "SAlowHierarchy"] == "B")[, 1],"SAid"
+  ]
+  lowerC <- rdbesPrepObject[["SA"]][
+    (rdbesPrepObject[["SA"]][, "SAlowHierarchy"] == "C")[, 1],"SAid"
+  ]
+  lowerD <- rdbesPrepObject[["SA"]][
+    (rdbesPrepObject[["SA"]][, "SAlowHierarchy"] == "D")[, 1],"SAid"
+  ]
+
+  # Join FM and BV - we do it in both ways left join on FM and right join on
+  # BV.  This is because not all of the lower hierachies have FM data
+
+  fMBV <-
+    dplyr::left_join(rdbesPrepObject[["FM"]],
+                     rdbesPrepObject[["BV"]],
+                     by = "FMid"
+    )
+  # sort out the wrong SAid column name after the join
+  names(fMBV)[names(fMBV) == "SAid.x"] <- "SAid"
+  fMBV[, "SAid.y" := NULL]
+
+  # Now get rid of any unneccessary id fields to avoid trouble with field naming
+  allowedIds <- c("SAid", "FMid", "BVid")
+  unallowedIDs <-
+    paste0(names(rdbesPrepObject), "id")[
+      !(paste0(names(rdbesPrepObject), "id") %in% allowedIds)
+    ]
+  unallowedIDs <-
+    names(fMBV)[names(fMBV) %in% unallowedIDs]
+  if (length(unallowedIDs) > 0) fMBV[, (unallowedIDs) := NULL]
+
+
+  bVFM <- dplyr::right_join(rdbesPrepObject[["FM"]],
+                            rdbesPrepObject[["BV"]],
+                            by = "FMid"
+  )
+  # sort out the wrong SAid column name after the join
+  names(bVFM)[names(bVFM) == "SAid.y"] <- "SAid"
+  bVFM[, "SAid.x" := NULL]
+
+  # Now get rid of any unneccessary id fields to avoid trouble with field naming
+  allowedIds <- c("SAid", "FMid", "BVid")
+  unallowedIDs <-
+    paste0(names(rdbesPrepObject), "id")[
+      !(paste0(names(rdbesPrepObject), "id") %in% allowedIds)
+    ]
+  unallowedIDs <-
+    names(bVFM)[names(bVFM) %in% unallowedIDs]
+  if (length(unallowedIDs) > 0) bVFM[, (unallowedIDs) := NULL]
+
+  lowerA <- dplyr::left_join(lowerA, fMBV, by = "SAid")
+  lowerB <- dplyr::left_join(lowerB, fMBV, by = "SAid")
+  # Note the difference in lowerC
+  lowerC <- dplyr::left_join(lowerC, bVFM, by = "SAid")
+  lowerD <- dplyr::left_join(lowerD, fMBV, by = "SAid")
+
+  # Ok, this shoudl be all the FM, and BV data for all lower hierarchies
+  allLower <- rbind(lowerA, lowerB, lowerC, lowerD)
+
+  allLower
+
+}
+
+#' Private function to process the upper hierarchies when creating
+#' the RDBESEstObject
+#'
+#' @param myRDBESEstObj
+#' @param rdbesPrepObject
+#' @param hierarchyToUse
+#' @param i
+#'
+#' @return
+#'
+#' @examples
+processRDBESEstObjUpperHierarchies <- function(myRDBESEstObj = NULL,
+                                               rdbesPrepObject,
+                                               hierarchyToUse,
+                                               i = 1){
+
+  # Which tables do we expect to find in the data?
+  targetTables <- icesRDBES::tablesInRDBESHierarchies[[hierarchyToUse]]
+  thisTable <- targetTables[i]
+  print(thisTable)
+
+  if (thisTable %in% c("FM","BV")){
+    # if we've got to FM or BV we're done so lets stop
+    return (myRDBESEstObj)
+  } else {
+
+    # if we don't have an Est object yet we must be at the start - let's go!
+    if(is.null(myRDBESEstObj)){
+      myRDBESEstObj <- rdbesPrepObject[[thisTable]]
+
+    } else {
+      # If we already have an Est object let's join the new data to it
+
+      # get the sampling unit level
+      suLevel <- paste0("su",i-2)
+
+      if (i > 1) {
+        joinField <- paste0(targetTables[i - 1], "id")
+      } else {
+        joinField <- NA
+      }
+
+      # First get rid of any unneccessary id fields to avoid trouble
+      # with field naming
+      allowedIds <- c(paste0(thisTable, "id"), joinField, "VDid", "SLid")
+      unallowedIDs <-
+        paste0(names(rdbesPrepObject), "id")[
+          !(paste0(names(rdbesPrepObject), "id") %in% allowedIds)
+        ]
+      unallowedIDs <-
+        names(rdbesPrepObject[[thisTable]])[
+          names(rdbesPrepObject[[thisTable]]) %in% unallowedIDs
+        ]
+      if (length(unallowedIDs) > 0) {
+        rdbesPrepObject[[thisTable]][, (unallowedIDs) := NULL]
+      }
+
+      # Field names of the design variables
+      designVariables <- c("stratification","stratumName","clustering",
+                           "clusterName","sampler","numTotal","numSamp","selProb",
+                           "incProb","selectMeth","unitName", "selectMethCluster",
+                           "numTotalClusters","numSampClusters","selProbCluster",
+                           "incProbCluster","samp", "noSampReason")
+
+      # if this table has design variable columns
+      if (any(names(rdbesPrepObject[[thisTable]])
+              %in% paste0(thisTable,designVariables))){
+
+        #Rename design variables using su ("sampling unit") style names
+        # (we'll just ignore any of the "old" names that aren't present)
+        data.table::setnames(rdbesPrepObject[[thisTable]],
+                             old = paste0(thisTable,designVariables),
+                             new = paste0(suLevel,designVariables),
+                             skip_absent = TRUE)
+
+
+        # Add a column showing which table this sampling unit is from
+        suTable <- paste0(suLevel,"table")
+        rdbesPrepObject[[thisTable]][,suTable] <- thisTable
+      }
+
+      # Join this new table to the existing data
+      myRDBESEstObj <-
+        dplyr::left_join(myRDBESEstObj,
+                         rdbesPrepObject[[thisTable]],
+                         by = joinField)
+
+    }
+
+    # recursively call this function
+    processRDBESEstObjUpperHierarchies(myRDBESEstObj,
+                                       rdbesPrepObject,
+                                       hierarchyToUse,
+                                       i+1)
+  }
 }
