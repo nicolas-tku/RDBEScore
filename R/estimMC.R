3407e37c1b42d5798eb7f9482d91398f7385d063
David Currie <davidcurrie2001@yahoo.co.uk>
2022-09-01 12:59:27 +0100

    Accepts sel and inc probs as parameters so that UPSWR and UPSWOR can be calculated

--- R/estimMC.R
+++ R/estimMC.R
@@ -4,6 +4,8 @@
 #' @param sampled numeric total number of units sampled
 #' @param total numeric total number of units int the population
 #' @param method character selection method code e.g SRSWOR
+#' @param selProb the selection probabilities (if known)
+#' @param incProb the inclusion probabilities (if known)
 #'
 #' @return list of 7 elements including the population mean, total
 #' (and their variance), the algorithm name used and the I order
@@ -12,11 +14,14 @@
 #'
 #' @examples
 #' estimMC(c(3, 4, 4, 5), c(4, 4, 4, 4), c(8, 8, 8, 8))
-estimMC <- function(y, sampled, total, method = "SRSWOR") {
+estimMC <- function(y, sampled, total, method = "SRSWOR", selProb = NULL,
+                    incProb = NULL) {
   implementedMethods <- c(
     "^SRSWR$",
     "^SRSWOR$",
-    "^CENSUS$"
+    "^CENSUS$",
+    "^UPSWR$",
+    "^UPSWOR$"
   )
   if (!any(sapply(implementedMethods, grepl, method))) {
     stop(paste("Sampling method ", method, "not implemented!"))
@@ -33,7 +38,6 @@ estimMC <- function(y, sampled, total, method = "SRSWOR") {
   # TODO include also quasi random
   if (grepl("^SRS", method) || grepl("^CENSUS$", method)) {
     enk <- sampled / total
-    enl <- t(enk)
     if (grepl("WOR$", method) || grepl("^CENSUS$", method)) {
       enkl <- enk %*% t((sampled - 1) / (total - 1))
       diag(enkl) <- enk
@@ -44,9 +48,21 @@ estimMC <- function(y, sampled, total, method = "SRSWOR") {
     }
   }
 
-  # TODO Add Unequal probability here
+
   if (grepl("^UPS", method)) {
-    stop("TODO")
+
+    if (grepl("WOR$", method)) {
+      enk <- incProb
+      print(paste0("UPSWOR variance calculation requires second order",
+      " inclusion probabilities which are not available in the RDBES -",
+      " variance cannot be calculated"))
+      enkl <- NA
+    }
+
+    if (grepl("WR$", method)) {
+      enk <- n * selProb
+      enkl <- enk %*% t((n-1) * selProb)
+    }
   }
 
   # Generalized Horvitz-Thompson estimator
@@ -60,10 +76,11 @@ estimMC <- function(y, sampled, total, method = "SRSWOR") {
   varFunction <- function(y,enk,enkl){
     # Sen-Yates-Grundy estimate of variance
     var.algorithm <- "Sen-Yates-Grundy"
+    if (length(enkl) == 1 && is.na(enkl)) return(NA) # NA if we don't have enkl
     partY <- (y / enk) %*% t(rep(1,n)) - t((y / enk) %*% t(rep(1,n)) )
     diag(partY) <- NA
     partY <- partY^2
-    var.mat <- 0.5 * (((enk %*% enl) - enkl) / enkl)
+    var.mat <- 0.5 * (((enk %*% t(enk)) - enkl) / enkl)
     diag(var.mat) <- NA
     var.mat <- var.mat * partY
     var.total <- sum(var.mat, na.rm = T)
