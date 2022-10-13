997f34cc8bcd15cbb51ec1f65f198c3f955e2020
Petri Sarvamaa <petri.sarvamaa@luke.fi>
2022-10-13 10:54:44 +0300

    #56 Update validateRDBESDataObject tests

--- tests/testthat/test-validateRDBESDataObject.R
+++ tests/testthat/test-validateRDBESDataObject.R
@@ -9,120 +9,111 @@ test_that("validateRDBESobject does not produce errors or warnings",  {
   expect_error(validateRDBESDataObject(objectToCheck = myEmptyObject,
                                       verbose = FALSE),NA)
 })
-test_that("validateRDBESDataObject returns T for valid empty object",  {
+test_that(
+  "validateRDBESDataObject returns empty object for valid empty object",  {
 
   myEmptyObject <- createRDBESDataObject()
   myReturn <- validateRDBESDataObject(objectToCheck = myEmptyObject,
                                      verbose = FALSE)
-  expect_true(myReturn)
+  expect_equal(myEmptyObject, myReturn)
 
 })
-test_that("validateRDBESDataObject returns T for valid object from H1 data",  {
+test_that(
+  "validateRDBESDataObject returns input object for valid object from H1 data",
+  {
 
   myObject <- createRDBESDataObject(rdbesExtractPath = "./h1_v_1_19_13")
   myReturn <- validateRDBESDataObject(objectToCheck = myObject,
                                      verbose = FALSE)
-  expect_true(myReturn)
+  expect_equal(myObject, myReturn)
 
 })
-test_that("validateRDBESDataObject returns T for valid object from H1 data (also checking data types)",  {
+test_that("validateRDBESDataObject returns same object for valid object from H1
+          data (also checking data types)",  {
 
   myObject <- createRDBESDataObject(rdbesExtractPath = "./h1_v_1_19_13")
   myReturn <- validateRDBESDataObject(objectToCheck = myObject,
                                      checkDataTypes = TRUE,
                                      verbose = FALSE)
-  expect_true(myReturn)
+  expect_equal(myObject, myReturn)
 
 })
-test_that("validateRDBESDataObject returns T for valid object from H5 data",  {
+test_that("validateRDBESDataObject returns same object for valid object from H5
+          data",  {
 
   myObject <- createRDBESDataObject(rdbesExtractPath = "./h5_v_1_19_13")
   myReturn <- validateRDBESDataObject(objectToCheck = myObject,
                                      verbose = FALSE)
-  expect_true(myReturn)
+  expect_equal(myObject, myReturn)
 
 })
-test_that("validateRDBESDataObject returns T for valid object from H5 data (also checking data types)",  {
+test_that("validateRDBESDataObject returns same object for valid object from H5
+          data (also checking data types)",  {
 
   myObject <- createRDBESDataObject(rdbesExtractPath = "./h5_v_1_19_13")
   myReturn <- validateRDBESDataObject(objectToCheck = myObject,
                                      checkDataTypes = TRUE,
                                      verbose = FALSE)
-  expect_true(myReturn)
+  expect_equal(myObject, myReturn)
 
 })
-test_that("validateRDBESDataObject returns F for NA",  {
+test_that("validateRDBESDataObject produces error for NA",  {
 
-  myReturn <- validateRDBESDataObject(objectToCheck = NA,
-                                     verbose = FALSE)
-  expect_false(myReturn)
+  expect_errorror(validateRDBESDataObject(objectToCheck = NA,
+                                     verbose = FALSE))
 
 })
-test_that("validateRDBESDataObject returns F for object that is not a list",  {
+test_that("validateRDBESDataObject produces error for object that is not a
+          list",  {
 
   myNonList <- data.frame (tableNames  = c("DE", "SE"))
-  myReturn <- validateRDBESDataObject(objectToCheck = myNonList,
-                                     verbose = FALSE)
-  expect_false(myReturn)
-
+  expect_error(validateRDBESDataObject(objectToCheck = myNonList,
+                                     verbose = FALSE))
 })
 
-test_that("validateRDBESDataObject returns F for object with extra name",  {
-
+test_that("validateRDBESDataObject produces error for object with extra name",
+          {
   myObject <- createRDBESDataObject(rdbesExtractPath = "./h1_v_1_19_13")
   myObject[['XX']] <- F
-  myReturn <- validateRDBESDataObject(objectToCheck = myObject,
-                                     verbose = FALSE)
-  expect_false(myReturn)
-
+  expect_error(validateRDBESDataObject(objectToCheck = myObject,
+                                     verbose = FALSE))
 })
-test_that("validateRDBESDataObject returns F for object without all names",  {
-
+test_that("validateRDBESDataObject produces error for object without all names",
+          {
   myObject <- createRDBESDataObject(rdbesExtractPath = "./h1_v_1_19_13")
   myObject[['DE']] <- NULL
-  myReturn <- validateRDBESDataObject(objectToCheck = myObject,
-                                     verbose = FALSE)
-  expect_false(myReturn)
-
+  expect_error(validateRDBESDataObject(objectToCheck = myObject,
+                                     verbose = FALSE))
 })
-test_that("validateRDBESDataObject returns F for object with a required field
-          removed",  {
-
+test_that("validateRDBESDataObject produces error for object with a required
+          field removed",  {
   myObject <- createRDBESDataObject(rdbesExtractPath = "./h1_v_1_19_13")
   myObject[['DE']]$DEsampScheme <- NULL
-  myReturn <- validateRDBESDataObject(objectToCheck = myObject,
-                                     verbose = FALSE)
-  expect_false(myReturn)
-
+  expect_error(validateRDBESDataObject(objectToCheck = myObject,
+                                     verbose = FALSE))
 })
 test_that("validateRDBESDataObject returns F for object with duplicate rows",  {
 
   myObject <- createRDBESDataObject(rdbesExtractPath = "./h1_v_1_19_13")
   myObject[['DE']] <- data.table::rbindlist(list(myObject[['DE']],myObject[['DE']]))
-  myReturn <- validateRDBESDataObject(objectToCheck = myObject,
-                                     verbose = FALSE)
-  expect_false(myReturn)
-
+  expect_error(validateRDBESDataObject(objectToCheck = myObject,
+                                     verbose = FALSE))
 })
 test_that("validateRDBESDataObject returns F for object with duplicate DEid values",  {
 
   myObject <- createRDBESDataObject(rdbesExtractPath = "./h1_v_1_19_13")
   myObject[['DE']][,"DEid"]  <- replicate(nrow(myObject[['DE']]),1)
-  myReturn <- validateRDBESDataObject(objectToCheck = myObject,
-                                     verbose = FALSE)
-  expect_false(myReturn)
-
+  expect_error(validateRDBESDataObject(objectToCheck = myObject,
+                                     verbose = FALSE))
 })
 test_that("validateRDBESDataObject returns F for object with invalid data types (when checking data types)",  {
 
   myObject <- createRDBESDataObject(rdbesExtractPath = "./h1_v_1_19_13")
   myObject[["DE"]]$DEid <- as.character(myObject[["DE"]]$DEid)
   myObject[["SD"]]$SDid <- as.character(myObject[["SD"]]$SDid)
-  myReturn <- validateRDBESDataObject(objectToCheck = myObject,
+  expect_error(validateRDBESDataObject(objectToCheck = myObject,
                                      checkDataTypes = TRUE,
-                                     verbose = FALSE)
-  expect_false(myReturn)
-
+                                     verbose = FALSE))
 })
 test_that("validateRDBESDataObject returns F for object with with duplicate DEid values and invalid data types (when checking data types)",  {
 
@@ -130,11 +121,9 @@ test_that("validateRDBESDataObject returns F for object with with duplicate DEid
   myObject[['DE']][,"DEid"]  <- replicate(nrow(myObject[['DE']]),1)
   myObject[["DE"]]$DEid <- as.character(myObject[["DE"]]$DEid)
   myObject[["SD"]]$SDid <- as.character(myObject[["SD"]]$SDid)
-  myReturn <- validateRDBESDataObject(objectToCheck = myObject,
+  expect_error(validateRDBESDataObject(objectToCheck = myObject,
                                      checkDataTypes = TRUE,
-                                     verbose = FALSE)
-  expect_false(myReturn)
-
+                                     verbose = FALSE))
 })
 
 test_that("validateRDBESDataObject prints empty table note",  {
