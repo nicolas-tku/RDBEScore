3b08f9e820e8ae38de4bf93d1dfefce237f52e78
nmprista <31890653+nmprista@users.noreply.github.com>
2022-10-13 14:39:58 +0300

    Revert "Merge branch 'dev' of https://github.com/ices-tools-dev/icesRDBES into dev"
    This reverts commit 945a85ef753474c96a3e2888fd6e3c8a61957fda, reversing
    changes made to 66b8bacb5d29e9b0ab949a2bed7d37d06b7cb914.

--- tests/testthat/test-combineRDBESDataObjects.R
+++ tests/testthat/test-combineRDBESDataObjects.R
@@ -3,41 +3,38 @@ capture.output({  ## suppresses printing of console output when running test()
 test_that("combineRDBESDataObjects returns error for NA",  {
 
     expect_error(combineRDBESDataObjects(RDBESDataObject1=NA,
-                                        RDBESDataObject2=NA),
-                 "objectToCheck is NA")
+                                        RDBESDataObject2=NA),"At least one of the RDBESDataObjects is not valid - mergeRDBESDataObjects will not proceed")
 })
 
-test_that("combineRDBESDataObjects returns invalid RDBESDataObject when
-          supplied with duplicate RDBESDataObjects",  {
+test_that("combineRDBESDataObjects returns invalid RDBESDataObject when supplied with duplicate RDBESDataObjects",  {
 
   myPath <- "./h1_v_1_19_13"
   myObject1 <- createRDBESDataObject(rdbesExtractPath = myPath)
   myObject2 <- createRDBESDataObject(rdbesExtractPath = myPath)
 
   # Check these are valid objects before we try and combine them
-  expect_error(validateRDBESDataObject(myObject1), NA)
-  expect_error(validateRDBESDataObject(myObject2), NA)
+  expect_true(validateRDBESDataObject(myObject1))
+  expect_true(validateRDBESDataObject(myObject2))
 
   myCombinedObject <- combineRDBESDataObjects(RDBESDataObject1=myObject1,
                                              RDBESDataObject2=myObject2)
 
-  expect_error(validateRDBESDataObject(myCombinedObject), "duplicate rows")
+  expect_false(validateRDBESDataObject(myCombinedObject))
 })
 
-test_that("combineRDBESDataObjects returns valid RDBESDataObject when supplied
-          with valid, different RDBESDataObjects",  {
+test_that("combineRDBESDataObjects returns valid RDBESDataObject when supplied with valid, different RDBESDataObjects",  {
 
   myObject1 <- createRDBESDataObject(rdbesExtractPath = "./h1_v_1_19_13")
   myObject2 <- createRDBESDataObject(rdbesExtractPath = "./h5_v_1_19_13")
 
   # Check these are valid objects before we try and combine them
-  expect_error(validateRDBESDataObject(myObject1), NA)
-  expect_error(validateRDBESDataObject(myObject2), NA)
+  expect_true(validateRDBESDataObject(myObject1))
+  expect_true(validateRDBESDataObject(myObject2))
 
   myCombinedObject <- combineRDBESDataObjects(RDBESDataObject1=myObject1,
                                              RDBESDataObject2=myObject2)
 
-  expect_error(validateRDBESDataObject(myCombinedObject), NA)
+  expect_true(validateRDBESDataObject(myCombinedObject))
 })
 
 }) ## end capture.output
