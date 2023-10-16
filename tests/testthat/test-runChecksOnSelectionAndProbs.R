c9bb07a295060003954388e5fdb654c640dea4fe
David Currie <davidcurrie2001@yahoo.co.uk>
2023-10-16 17:09:25 +0200

    Fixed (needed to subset H1 data)

--- tests/testthat/test-runChecksOnSelectionAndProbs.R
+++ tests/testthat/test-runChecksOnSelectionAndProbs.R
@@ -2,16 +2,25 @@ capture.output({  ## suppresses printing of console output when running test()
 
 test_that("runChecksOnSelectionAndProbs runs without errors when stratification is present",  {
 
-  myPath <- "./h1_v_1_19_13"
+  myPath <- "./h1_v_1_19_18"
   myObject <- createRDBESDataObject(rdbesExtractPath = myPath)
+  # Only use the non-clustered test data
+  myObject <- filterRDBESDataObject(myObject,c("DEstratumName"),c("DE_stratum1_H1","DE_stratum2_H1","DE_stratum3_H1"))
+  myObject <- findAndKillOrphans(myObject, verbose = FALSE)
+
   myObject[["SA"]]$SAlowHierarchy <- "D"
+
   expect_error(runChecksOnSelectionAndProbs(myObject),NA)
 
 })
   test_that("runChecksOnSelectionAndProbs runs without errors when stratification is present - different numTotal for different strata",  {
 
-    myPath <- "./h1_v_1_19_13"
+    myPath <- "./h1_v_1_19_18"
     myObject <- createRDBESDataObject(rdbesExtractPath = myPath)
+    # Only use the non-clustered test data
+    myObject <- filterRDBESDataObject(myObject,c("DEstratumName"),c("DE_stratum1_H1","DE_stratum2_H1","DE_stratum3_H1"))
+    myObject <- findAndKillOrphans(myObject, verbose = FALSE)
+
     myObject[["SA"]]$SAlowHierarchy <- "D"
     myObject[["VS"]][myObject[["VS"]]$VSstratumName == "VS_stratum2","VSnumTotal"] <- 40
     expect_error(runChecksOnSelectionAndProbs(myObject),NA)
@@ -19,8 +28,12 @@ test_that("runChecksOnSelectionAndProbs runs without errors when stratification 
   })
 test_that("runChecksOnSelectionAndProbs runs without errors when stratification is present - different numSampled for different strata",  {
 
-  myPath <- "./h1_v_1_19_13"
+  myPath <- "./h1_v_1_19_18"
   myObject <- createRDBESDataObject(rdbesExtractPath = myPath)
+  # Only use the non-clustered test data
+  myObject <- filterRDBESDataObject(myObject,c("DEstratumName"),c("DE_stratum1_H1","DE_stratum2_H1","DE_stratum3_H1"))
+  myObject <- findAndKillOrphans(myObject, verbose = FALSE)
+
   myObject[["SA"]]$SAlowHierarchy <- "D"
   myObject[["VS"]][myObject[["VS"]]$VSstratumName == "VS_stratum2","VSnumSamp"] <- 2
   expect_error(runChecksOnSelectionAndProbs(myObject),NA)
