0eb7d46c5df64e72ded0e9887af868dc835587ba
nicholascarey <nicholascarey@gmail.com>
2023-10-17 09:45:50 +0200

    rename importRDBESDownloadData to importRDBESDataZIP

--- R/createRDBESDataObject.R
+++ R/createRDBESDataObject.R
@@ -23,135 +23,31 @@
 #'
 #' @examples
 #' myEmptyRDBESObject <- importRDBESDataCSV()
-createRDBESDataObject <- function(rdbesExtractPath = NA,
-                                 listOfFileNames = NA,
-                                 castToCorrectDataTypes = TRUE) {
 
-#
-#   # If we have not been passed a list of file names use a default
-#   if (length(listOfFileNames) == 1 && is.na(listOfFileNames)){
-#
-#     # A named list with the file names that are produced by the RDBES download
-#     fileNames <- list(
-#       "DE" = "Design",
-#       "SD" = "SamplingDetails",
-#       "VS" = "VesselSelection",
-#       "FT" = "FishingTrip",
-#       "FO" = "FishingOperation",
-#       "TE" = "TemporalEvent",
-#       "LO" = "Location",
-#       "OS" = "OnshoreEvent",
-#       "LE" = "LandingEvent",
-#       "SS" = "SpeciesSelection",
-#       "SA" = "Sample",
-#       "FM" = "FrequencyMeasure",
-#       "BV" = "BiologicalVariable",
-#       "VD" = "VesselDetails",
-#       "SL" = "SpeciesList",
-#       "CL" = "CommercialLanding",
-#       "CE" = "CommercialEffort"
-#     )
-#
-#     # Stick ".csv" on to each default name
-#     fileNames <-lapply(fileNames, function(x){paste(x,".csv",sep="")})
-#
-#   } else {
-#     fileNames <- listOfFileNames
-#   }
-#
-#   # Create a named list using the short names - set all values to NULL
-#   myList <- stats::setNames(
-#     as.list(replicate(length(fileNames), NULL)),
-#     names(fileNames)
-#   )
-#
-#
-#   # If we have been supplied with a path to files we will try and read them
-#   # otherwise we'll just return an empty rdbesRawBoject
-#   if (!is.na(rdbesExtractPath)) {
-#
-#     # Determine the files which actually exist
-#     filesWhichExist <- names(
-#       fileNames[file.exists(
-#         paste(rdbesExtractPath, "/", fileNames, sep = "")
-#       )]
-#     )
-#
-#     # If we don't find relevent files in the dir give a warning
-#     if (length(filesWhichExist) == 0) {
-#       warning(paste0("No relevent files found in given directory",
-#       " - an empty object will be created"))
-#     } else {
-#
-#       # Read the files which exist
-#       for (myFile in filesWhichExist) {
-#         # Read the file
-#         myList[[myFile]] <-
-#           utils::read.csv(
-#             paste(rdbesExtractPath, "/", fileNames[myFile],  sep = ""),
-#             header = TRUE, sep = ",", stringsAsFactors = FALSE
-#           )
-#
-#         # Change each entry to a data table
-#         #myList[[myFile]] <-
-#         data.table::setDT(myList[[myFile]])
-#
-#         # Change database field names to R names where we can
-#         myNames <- RDBEScore::mapColNamesFieldR[
-#           RDBEScore::mapColNamesFieldR$Table.Prefix == myFile, ]
-#         myNameMatches <- match(
-#           trimws(tolower(names(myList[[myFile]]))),
-#           trimws(tolower(myNames$Field.Name))
-#         )
-#         myNameMatchesNotNA <- myNameMatches[!is.na(myNameMatches)]
-#         names(myList[[myFile]])[!is.na(myNameMatches)] <-
-#           myNames[myNameMatchesNotNA, "R.Name"]
-#       }
-#     }
-#
-#     ## DATA FIX - spelling mistake in 1 download file format ...
-#     if ("CLincidentialByCatchMitigationDevice" %in%
-#         names(myList[["CL"]])) {
-#       data.table::setnames(
-#         myList[["CL"]]
-#         , "CLincidentialByCatchMitigationDevice"
-#         , "CLIBmitiDev")
-#     }
-#
-#   }
-#
-#   # Create an RDBESDataObject using the constructor
-#   myRDBESDataObject <- RDBEScore::newRDBESDataObject(DE = myList[["DE"]],
-#                                         SD = myList[["SD"]],
-#                                         VS = myList[["VS"]],
-#                                         FT = myList[["FT"]],
-#                                         FO = myList[["FO"]],
-#                                         TE = myList[["TE"]],
-#                                         LO = myList[["LO"]],
-#                                         OS = myList[["OS"]],
-#                                         LE = myList[["LE"]],
-#                                         SS = myList[["SS"]],
-#                                         SA = myList[["SA"]],
-#                                         FM = myList[["FM"]],
-#                                         BV = myList[["BV"]],
-#                                         VD = myList[["VD"]],
-#                                         SL = myList[["SL"]],
-#                                         CL = myList[["CL"]],
-#                                         CE = myList[["CE"]])
-#
-#
-#   if (castToCorrectDataTypes){
-#     # Ensure all the columns are the correct data type
-#     myRDBESDataObject <- setRDBESDataObjectDataTypes(myRDBESDataObject)
-#   }
-#
-#   # Set a key on any data tables in myList - use the XXid column as the key
-#   for(aTable in names(myRDBESDataObject)){
-#     if ('data.table' %in% class(myRDBESDataObject[[aTable]])){
-#       data.table::setkeyv(myRDBESDataObject[[aTable]],paste0(aTable,"id"))
-#     }
-#   }
-#
-#   # Return the object
-#   myRDBESDataObject
+createRDBESDataObject <- function(x = NULL) {
+  #listOfFileNames = NA,
+  #castToCorrectDataTypes = TRUE)
+
+  #  x <- "tests/testthat/h1_v_1_19_18/2023_10_16_104555.zip"
+
+  # if input is string and zip file
+  if(is.character(x) && grepl(".zip", x)) import.type <- "zip" else
+    # if input is string and folder assume it contains only csv files
+    if(is.character(x) && file_test("-d", x)) import.type <- "folder" else
+      # if input is string and folder assume it contains only csv files
+      if(is.list(x)) import.type <- "list" else
+        stop("x input type not recognised. Should be a RDBES zip file, folder of csv files, or list of data frames.")
+
+
+      # -------------------------------------------------------------------------
+
+      if(import.type == "zip") importRDBESDataZIP()
+
+
+      if(import.type == "folder") importRDBESDataCSV()
+
+
+      if(import.type == "list") importRDBESDataCSV()
+
+
 }
