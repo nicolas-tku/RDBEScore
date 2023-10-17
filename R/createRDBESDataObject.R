5cb40e039da0ce5c186b01efd74a6dd9c5dcd558
nicholascarey <nicholascarey@gmail.com>
2023-10-17 10:04:06 +0200

    Importing - Minor update

--- R/createRDBESDataObject.R
+++ R/createRDBESDataObject.R
@@ -1,30 +1,33 @@
-#' Create an RDBES Raw Object
+#' Create an RDBES Data Object
 #'
-#' Create an R object from a folder containing .csv data files downloaded from
-#' the RDBES upload/download page.
+#' Create a RDBES Data object from either:
+#'  - a `zip` file downloaded from RDBES (or multiple zip files if you want to include CL and CE data)
+#'  - a folder containing `csv` files downloaded from RDBES (e.g. the unzipped archive file)
+#'  - a `list` of data frames in the current environment representing different tables in the hierarchy
 #'
-#' @param rdbesExtractPath (Optional) The path to the csv files produced as an
-#' extract by the ICES RDBES.  If no path is suppled then an empty
-#' RDBESDataObject will be returned.
+#' @param x The path to the zip file extracted from RDBES, or path to a folder
+#'   of `csv` files, or a list object in the current environment containing data frames of each table.
+#'
 #' @param listOfFileNames (Optional) A named list of file names - the list names
-#' shoudl be the two-letter code for the relevent table e.g.
-#' list("DE" = "DE.csv",... ).  If the parameter is not supplied then the
-#' default file names used by the RDBES data download will be used e.g.
-#' "Design.csv" etc.
-#' @param castToCorrectDataTypes (Optional) If TRUE then the function
-#' will attempt to cast the required columns to the correct data type.  If
-#' FALSE then the column data types will be determined by how the csv files
-#' are read in.  The default is TRUE
+#'   shoudl be the two-letter code for the relevent table e.g. list("DE" =
+#'   "DE.csv",... ).  If the parameter is not supplied then the default file
+#'   names used by the RDBES data download will be used e.g. "Design.csv" etc.
+#' @param castToCorrectDataTypes (Optional) If TRUE then the function will
+#'   attempt to cast the required columns to the correct data type.  If FALSE
+#'   then the column data types will be determined by how the csv files are read
+#'   in.  The default is TRUE
 #'
 #' @return A RDBESDataObject.  If a path to RDBES extract files is provided then
-#' it will contain the data from those files.  If no path is supplied then
-#' an empty RDBESDataObject will be returned.
+#'   it will contain the data from those files.  If no path is supplied then an
+#'   empty RDBESDataObject will be returned.
 #' @export
+#' @md
 #'
 #' @examples
 #' myEmptyRDBESObject <- importRDBESDataCSV()
 
 createRDBESDataObject <- function(x = NULL) {
+
   #listOfFileNames = NA,
   #castToCorrectDataTypes = TRUE)
 
@@ -47,7 +50,10 @@ createRDBESDataObject <- function(x = NULL) {
       if(import.type == "folder") importRDBESDataCSV()
 
 
-      if(import.type == "list") importRDBESDataCSV()
+      if(import.type == "list") {
+        warning("NOTE: creating RDBES objects from list of data frames bypasses the ICES data checks. Make sure you know what you are doing.")
+        importRDBESDataDFS()s
+      }
 
-
+      # check for duplicate tables
 }
