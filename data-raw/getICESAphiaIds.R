d6c053bd4605ef16f4b8105fc7245e32bde29689
David Currie <davidcurrie2001@yahoo.co.uk>
2023-10-18 16:55:58 +0200

    Change key to a char so it is easier to join with SAspeCode

--- data-raw/getICESAphiaIds.R
+++ data-raw/getICESAphiaIds.R
@@ -3,6 +3,9 @@
 #codeTypes <- icesVocab::getCodeTypeList()
 icesSpecWoRMS <- icesVocab::getCodeList(code_type = "SpecWoRMS")
 
+# Change the key to a char (so it is easier to join to the RDBES speCode field)
+icesSpecWoRMS$Key <- as.character(icesSpecWoRMS$Key)
+
 # Add a date field
 icesSpecWoRMS$DateDownloaded <- Sys.Date()
 
